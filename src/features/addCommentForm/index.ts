d4f1ec48938ef1b436d0c1d8c6f0294f99e4a929
Nikita Galanzovskiy <niopica@mail.ru>
2024-11-20 23:21:35 +0500

    add article selector, new icon

--- src/features/addCommentForm/index.ts
+++ src/features/addCommentForm/index.ts
@@ -1,4 +1,4 @@
 export { AddCommentFormSchema } from './model/types/addCommentForm';
 export {
     AddCommentFormAsync as AddCommentForm,
-} from './ui/AddCommentForm/AddCommentForm.async';
+} from './ui/addCommentForm/AddCommentForm.async';
