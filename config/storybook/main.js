fe55bb03b5f97a7ded8a710cb9eea927fb5a68ba
Nikita Galanzovskiy <niopica@mail.ru>
2025-01-13 22:10:10 +0500

    add theme addon for storybook

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -4,9 +4,16 @@ module.exports = {
     ],
     addons: [
         '@storybook/addon-links',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                backgrounds: false,
+            },
+        },
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
         'storybook-addon-mock/register',
+        'storybook-addon-themes',
     ],
     framework: '@storybook/react',
     core: {
